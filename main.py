0f104daaef28c4484f2185865e673a7edc1882b0
jhorman jesus castellanos morales <160280241+Jhormancastella@users.noreply.github.com>
2024-08-08 17:04:46 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import modules.corefiles as cf
+import modules.corefilesP as cfp
+import funciones.globales as fg
+import ui.uiMenuventa as uiSp
+import ui.uiMenucompras as uiPt
+import ui.uiMenuRegistro as uiC
+
+def mainMenu(op):
+    fg.borrar_pantalla()
+    title = """
+    🥖🍞🥐🥨🥪🥯🥖🍞🥐🥨🥪🥯🥖🍞🥐🥨🥪🥯🥖🍞🥐🥨🥪🥯🥐🥨🥪🥯🥖🍞🥐🥨🥪🥯
+    🥖 👩🏻‍🍳👩🏻‍🍳👩🏻‍🍳👩🏻‍🍳👩🏻‍🍳 PanCamp 👩🏽‍🍳👩🏽‍🍳👩🏽‍🍳👩🏽‍🍳👩🏽‍🍳 🥖
+    🥖🍞🥐🥨🥪🥯🥖🍞🥐🥨🥪🥯🥖🍞🥐🥨🥪🥯🥖🍞🥐🥨🥪🥯🥐🥨🥪🥯🥖🍞🥐🥨🥪🥯
+    """
+    mainMenuOp = "1. Menu venta\n2. Menu Compras\n3. Menu Registro\n4. Salir"
+    if op != 4:
+        print(title)
+        print(mainMenuOp)
+        try:
+            opcion = int(input(':) '))
+        except ValueError:
+            print('Error en la opcion ingresada')
+            fg.pausar_pantalla()
+            mainMenu(0)
+        else:
+            if opcion == 1:
+                uiSp.MenuEspecialista(0)
+            elif opcion == 2:
+                uiPt.MenuPacientes(0)
+            elif opcion == 3:
+                uiC.MenuConsulta(0)
+            elif opcion == 4:
+                print("Regrese pronto ....")
+                fg.pausar_pantalla()
+            else:
+                print('Opcion ingresada no pertenece al menu de opciones')
+                fg.pausar_pantalla()
+                mainMenu(0)
+
+if __name__ == '__main__':
+    cf.MY_DATABASE = 'data/pacientes.json'
+    cfp.MY_DATABASEP = 'data/especialistas.json'
+    cf.checkFile(fg.centroClinico)
+    cfp.checkFile(fg.centroClinico)
+    mainMenu(0)
---
+++ readme.md
@@ -0,0 +1,37 @@
+# taller de python
+
+## Propósito
+Este proyecto tiene como objetivo desarrollar de un programa en pytho que 
+ayude con la gestion de ventas y compras y reportes.
+
+## clonar repositorio
+```bash
+
+```
+
+## codigo en base a 
+- carpetas
+- - `main.py`: archivos con los ejercisios.
+- - `datos.json`: archivos con los ejercisios.
+- - `generacion_reportes.py`: archivos con los ejercisios.
+- - `gestion_compras.py`: archivos con los ejercisios.
+- - `gestion_ventas.py`: archivos con los ejercisios.
+
+
+Hecho por (jhorman jesus castellanos morales )
+
+> [!NOTE]
+> puede que no sirva
+
+> [!TIP]
+> 
+
+> [!IMPORTANT]  
+> se encuntra culminado 
+
+> [!WARNING]  
+> 
+
+> [!CAUTION]
+> 
+
